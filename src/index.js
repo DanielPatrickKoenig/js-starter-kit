34c455ecd106189585455e6a4be0104e4d16de63
Dan Koenig <dan.koenig@helloworld.com>
2021-04-10 18:58:56 -0400

    finilize for submission

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const chartMargin = {top: 10, bottom : 15, left: 130, right: 10};
 const waveCount = 6;
 let chartSize = {width: 0, height: 0};
 let barValues = [];
-let labelContainer;
+let sideLabelContainer;
+let bottomLabelContainer;
 const labelMatrix = [
   [1, 3], 
   [2, 3], 
@@ -91,29 +92,38 @@ function getHighestValue(){
 }
 function render () {
   let maxVal = getHighestValue();
-  const highest = createLabels(maxVal);
+  const highest = createSideLabels(maxVal);
   let keys = Object.keys(data);
   for(let i = 0; i < keys.length; i++){
     let height = (data[keys[i]].value / highest) * chartSize.height;
     // barValues.push({ height });
     if (barValues[i].height != height) {
       TweenLite.to(barValues[i], .5, {height});
-      barValues[i].intensity = (Math.random() * .75) + 1.6;
+      const calculatedIntensity = (Math.random() * .75) + 1.6;
+      barValues[i].intensity = calculatedIntensity < height ? calculatedIntensity : height;
     }
   }
+  createBottomLabels();
+}
+
+function getDimensions (index) {
+  const footprint = 1 / Object.keys(data).length;
+  const offset = chartSize.width * footprint * index;
+  const barWidth = chartSize.width * footprint * .8;
+  return { footprint, offset, barWidth };
 }
 
 function redraw (barVal, index) {
-  const footprint = 1 / Object.keys(data).length;
-  let offset = chartSize.width * footprint * index;
-  let barWidth = chartSize.width * footprint * .8;
+  const dimensions = getDimensions(index);
+  // let offset = chartSize.width * footprint * index;
+  // let barWidth = chartSize.width * footprint * .8;
   let points = [
-    {x: chartMargin.left + barWidth + offset, y: chartMargin.top + chartSize.height},
-    {x: chartMargin.left + offset, y: chartMargin.top + chartSize.height}
+    {x: chartMargin.left + dimensions.barWidth + dimensions.offset, y: chartMargin.top + chartSize.height},
+    {x: chartMargin.left + dimensions.offset, y: chartMargin.top + chartSize.height}
   ];
   for(let i = 0; i < barVal.wave.length; i++){
     let point =  {
-      x: chartMargin.left + ((barWidth / (barVal.wave.length - 1)) * i) + offset, 
+      x: chartMargin.left + ((dimensions.barWidth / (barVal.wave.length - 1)) * i) + dimensions.offset, 
       y: chartMargin.top + chartSize.height - barVal.height + barVal.wave[i].y,
     };
     if(i > 0){
@@ -121,7 +131,7 @@ function redraw (barVal, index) {
     }
     points.push(point);
   }
-  let bar = createBar(colors[index], points, bars[index], (barWidth / waveCount) / 2);
+  let bar = createBar(colors[index], points, bars[index], (dimensions.barWidth / waveCount) / 2);
   if(!bars[index]){
     bars.push(bar);
     app.stage.addChild(bar);
@@ -129,16 +139,16 @@ function redraw (barVal, index) {
   if(barValues[index].intensity > 0){
     barValues[index].intensity -= .0002;
   }
-  app.stage.addChild(labelContainer);
+  app.stage.addChild(sideLabelContainer);
 }
 
-function createLabels (max) {
-  if(!labelContainer){
-    labelContainer = new PIXI.Sprite();
-    app.stage.addChild(labelContainer);
+function createSideLabels (max) {
+  if(!sideLabelContainer){
+    sideLabelContainer = new PIXI.Sprite();
+    app.stage.addChild(sideLabelContainer);
   }
-  for (let i = labelContainer.children.length; i >= 0; i--) {
-    labelContainer.removeChild(labelContainer.children[i]);
+  for (let i = sideLabelContainer.children.length; i >= 0; i--) {
+    sideLabelContainer.removeChild(sideLabelContainer.children[i]);
   }
   let labelIndex = 0;
   for (let i = 0; i < labelMatrix.length; i++) {
@@ -149,15 +159,35 @@ function createLabels (max) {
   for (let i = 0; i <= labelMatrix[labelIndex][1]; i++) {
     let label = new PIXI.Text((labelMatrix[labelIndex][0] / (labelMatrix[labelIndex][1] - 1)) * i,{fontFamily : 'Arial', fontSize: '12', fill: 0x000000, align: 'left'});
     label.y = (chartMargin.top + chartSize.height) - (chartSize.height / (labelMatrix[labelIndex][1] - 1) * i);
-    labelContainer.addChild(label);
+    sideLabelContainer.addChild(label);
     let g = new PIXI.Graphics();
     g.lineStyle(1,0x000000,1);
     g.moveTo(0, 0);
     g.lineTo(document.querySelector('.pixi-stage').getBoundingClientRect().width, 0);
     g.y = label.y;
-    labelContainer.addChild(g);
+    sideLabelContainer.addChild(g);
   }
   return labelMatrix[labelIndex][0];
+}
+
+function createBottomLabels (width) {
+  if (!bottomLabelContainer) {
+    bottomLabelContainer = new PIXI.Sprite();
+    app.stage.addChild(bottomLabelContainer);
+  }
+  for (let i = bottomLabelContainer.children.length; i >= 0; i--) {
+    bottomLabelContainer.removeChild(bottomLabelContainer.children[i]);
+  }
+  let keys = Object.keys(data);
+  for(let i = 0; i < keys.length; i++){
+    const dimensions = getDimensions(i);
+    let label = new PIXI.Text(data[keys[i]].label, {fontFamily : 'Arial', fontSize: '12', fill: 0x000000});
+    label.x = chartMargin.left + dimensions.offset + (dimensions.barWidth / 2);
+    label.y = chartMargin.top + chartSize.height;
+    label.anchor.x = 0.5;
+    bottomLabelContainer.addChild(label);
+  }
+  
 }
 
 function createBar (color, path, graphic, spread) {
